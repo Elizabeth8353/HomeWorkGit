38735848e6f584ac4d40c254342dc96cda2b3bb8
Elizabeth8353 <evaldivieso6@gmail.com>
2021-08-13 22:21:25 -0400

    First Commit Integers 1 to 10

---
+++ src/FirstClass.java
@@ -0,0 +1,13 @@
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// Print integers from 1 to 10
+		
+		for(int i=1;i<=10;i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
