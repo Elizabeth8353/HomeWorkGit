caefc932ed8f98fd77215d25b7067509f667b976
Elizabeth8353 <evaldivieso6@gmail.com>
2021-08-13 22:43:07 -0400

    Second Commit 1 to 100

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -4,7 +4,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		// Print integers from 1 to 10
 		
-		for(int i=1;i<=10;i++) {
+		for(int i=1;i<=100;i++) {
 			System.out.println(i);
 		}
 
